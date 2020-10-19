c00f9c31d0d12b3ed0031ab9e1b7530a24fff7dc
angnkv22 <angel.neikov@gmail.com>
2020-10-19 19:38:22 +0000

    Neu

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -1,28 +1,33 @@
 package exercise3;
 
-import java.util.Scanner;
+  import java.util.Scanner;
 
-public class NumberCounter {
+  public class NumberCounter {
 
   public static void main(String[] args) {
-  Scanner input = new Scanner(System.in);
-  int count = 0;
-  int sum = 0;
-  int number = input.nextInt ();
-  do {
-            System.out.println("Enter a number: ");
-            a = input.nextInt();
-            b = input.nextInt();
-            boolean positive = (a > 0);
-            System.out.println("Positive number: " + positive);
-            boolean negative = (b)
-  while (number != 0) {
-  count++;
-  sum = sum + number;
-  number = input.nextInt();
-  }
-        System.out.println("Count: " + count );
-        System.out.println("Sum: " + sum );
+    Scanner input = new Scanner(System.in);
+
+        int positive = 0;
+        int negative = 0;
+        double sum = 0;
+        int average = 0;
+        int count = 0;
+        int number= input.nextInt();
+        while (number !=0 ){
+          if (number>0){
+            positive++;
+          } else {
+            negative++;
+          }
+          count++;
+          sum = sum + number;
+          number= input.nextInt();
+        }
+        System.out.println("Positive numbers: " + positive);
+        System.out.println("Negative numbers: " + negative);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + sum/count);
+      
     }
 
 }
--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -6,18 +6,19 @@ public class PrimeNumber {
 
   public static void main(String[] args) {
   Scanner input = new Scanner(System.in);
-  int number = input.nextInt();
-  int a = 2;
-  boolean prime = true;
-  while (a < number) {
-    if (number % a == 0) {
-    prime = false;
-    break;
-  }
-  a++;
+ 
+    int number = input.nextInt();
+    int a = 2;
+    boolean prime = true;
+    while (a < number) {
+      if (number % a == 0) {
+      prime = false;
+      break;
+    }
+      a++;
     }
  
-  System.out.println("Prime number " + prime);
-  }
+  System.out.println("Prime number is " + prime);
+    }
 
-   }
+    }
